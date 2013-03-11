a020a2cf3984bdf1acca40863b38cdb5c2470c9f
ankitml <ankitml@gmail.com>
2013-03-11 14:43:00 +0530

    index.php is done

---
+++ index-processing.php
@@ -0,0 +1,10 @@
+<?php 
+if(isset($_POST["add-submit"])) {
+    echo "form submitted";
+} else {
+    echo "form not submitted";
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -2,14 +2,31 @@
 <head> 
   <title> Pensive 0.1 </title>
   <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.min.css" media="all">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" media="all">
 </head>
-<?php
-include 'lib/forms/form-init.php';
-?>
 <body>
   <div class="container">
-
-  </div>
+  <div class="row-fluid">
+  <p class="lead"> Pensive 0.1 </p>
+  <div class="span7">
+    <form id="add" method="post" action="index-processing.php">
+    <input type="text" name="add-title" class="input-block-level"  
+           placeholder="Title"><br>
+    <textarea class="input-block-level" rows="10" placeholder="Article" 
+              name="add-text"></textarea></br>
+    <input class="btn btn-success" type="submit" name="add-submit" 
+           value="Add New Article">
+    </form>
+  </div> <!-- span8 ending -->
+  <div class="span4">
+<!--    <a href=""> First Article </a></br>
+    <a href=""> Second Article </a></br>
+    <a href=""> Third Article </a></br>
+    <a href=""> 4th Article </a></br>
+    <a href=""> Five Article </a></br> -->
+  </div> <!-- span4 ending -->
+  </div> <!-- row-fluid ending -->
+  </div> <!-- container ending -->
   <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
