3cabff910ad21dcb893ad475786071ad0add6c48
ankitml <ankitml@gmail.com>
2013-09-11 01:02:11 +0530

    testing function is working, pending documentation

--- lib/test.php
+++ lib/test.php
@@ -4,17 +4,21 @@ include_once('../config.php');
 
 include_once('orm.php');
 
-function function_test($function_name, $input=null) {
+function function_test($function_name, $input=null, $errors=null) {
 //[Pending] - Echo test passed output false  / whatever is outcome and output failed when function breaks. Give option of giving inputs to funcitons also
 	if($input==null) {
 		$input = array();
+	}
+	if($errors==null) {
+		$errors = array(-1);
 	}
 	if(empty($input)) {
     	$output = $function_name();
 	} else {
 		$output = call_user_func_array($function_name, $input);
 	}
-    if($output) {
+
+    if(!in_array($output, $errors)) {
         echo 'test passed Outuput is :<pre>';
         print_r($output);
         echo '</pre>';
@@ -23,6 +27,10 @@ function function_test($function_name, $input=null) {
     }
 }
 
+// function one($one, $two, $three) {
+// 	return "hola . $one . $two" . serialize($three);
 
+// }
 
+// function_test("one", array("one", "two", array("gollum", "ate", "fish")));
 ?>
